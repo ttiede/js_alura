4e0fa2840ffa0ce4ebac04161aa43a9713fc014a
Tiago Tiede <geschopf@gmail.com>
2016-06-26 14:18:13 -0300

    Calcula refactoring using javascript anonymous function

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,33 +1,37 @@
 //calcula-imc.js
 
-var trsPacientes = document.getElementsByClassName("paciente");
+var botao = document.getElementById("calcula-imcs");
+botao.addEventListener("click", function() { //função anonima inves de calculaTodosImcs
+
+    var trsPacientes = document.getElementsByClassName("paciente");
 
 percorreArray(trsPacientes, function (pacienteTr){
 
-    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
-    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
-    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+        var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+        var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+        var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
 
-    var pacienteAtual = {
-        nome : tdNome.textContent,
-        peso : tdPeso.textContent,
-        altura : tdAltura.textContent,
-        pegaImc: function() {
+        var pacienteAtual = {
+            nome : tdNome.textContent,
+            peso : tdPeso.textContent,
+            altura : tdAltura.textContent,
+            pegaImc: function() {
 
-            if(this.altura != 0){
-                var imc = this.peso / (this.altura * this.altura);
-                return imc;
-            } else{
+                if(this.altura != 0){
+                    var imc = this.peso / (this.altura * this.altura);
+                    return imc;
+                } else{
 
-                console("Não posso dividir por zero!");
+                    console("Não posso dividir por zero!");
+                }
             }
-        }
-    };
+        };
 
-    var imc = pacienteAtual.pegaImc();
+        var imc = pacienteAtual.pegaImc();
 
-    var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
-    tdImc.textContent = imc;
+        var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+        tdImc.textContent = imc;
 
-    console.log(imc);
-}); 
+        console.log(imc);
+    });
+});
