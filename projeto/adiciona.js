cbe997e39b3a08f3edc89a2ecaa55a54c794ffd9
Tiago Tiede <geschopf@gmail.com>
2016-06-26 14:28:48 -0300

    Refactoring percorre js

---
+++ projeto/adiciona.js
@@ -0,0 +1,22 @@
+var botao = document.querySelector("#adicionar-paciente");
+botao.addEventListener("click", function(e){
+    e.preventDefault();
+
+    var campoNome = document.querySelector("#campo-nome");
+    var campoPeso = document.querySelector("#campo-peso");
+    var campoAltura = document.querySelector("#campo-altura");
+
+    var novoPaciente = "<tr class='paciente'>"+
+                "<td class='info-nome'>"+campoNome.value+"</td>"+
+                "<td class='info-peso'>"+campoPeso.value+"</td>"+
+                "<td class='info-altura'>"+campoAltura.value+"</td>"+
+                "<td class='info-imc'></td>"+
+             "</tr>";
+
+    var todosPacientes = document.querySelector("table");
+    todosPacientes.innerHTML +=  novoPaciente;
+
+    campoNome.value = "";
+    campoPeso.value = "";
+    campoAltura.value = "";
+});
